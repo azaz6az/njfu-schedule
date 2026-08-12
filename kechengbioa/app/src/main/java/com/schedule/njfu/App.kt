4d8c15f7956fbbf2722ceedc95157f556433f5ba
azaz6az <3075269531@qq.com>
2026-08-12 11:26:55 +0800

    feat: pre-class alarm reminders with boot reschedule

--- kechengbioa/app/src/main/java/com/schedule/njfu/App.kt
+++ kechengbioa/app/src/main/java/com/schedule/njfu/App.kt
@@ -1,11 +1,15 @@
 package com.schedule.njfu
 
 import android.app.Application
+import com.schedule.njfu.reminder.ReminderScheduler
 import com.schedule.njfu.widget.WidgetRefreshWorker
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         WidgetRefreshWorker.schedule(this)
+        GlobalScope.launch { ReminderScheduler.rescheduleToday(this@App) }
     }
 }
